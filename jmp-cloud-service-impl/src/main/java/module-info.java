f652995843d210a5a1dbb46c5ecb587484d840df
Helene-byte <51850053+Helene-byte@users.noreply.github.com>
2026-04-27 14:37:17 +0000

    chore: apply spotless formatting

--- jmp-cloud-service-impl/src/main/java/module-info.java
+++ jmp-cloud-service-impl/src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module jmp.cloud.service.impl {
-    requires transitive jmp.service.api;
-    requires jmp.dto;
-    requires static lombok;
-    requires jmp.cloud.bank.impl;
-    exports service.impl;
+  requires transitive jmp.service.api;
+  requires jmp.dto;
+  requires static lombok;
+  requires jmp.cloud.bank.impl;
 
+  exports service.impl;
 }
